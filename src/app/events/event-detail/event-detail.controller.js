4dd1b7e3ef37c7c88b2c24a248f2d8f29dc52c08
Luis Grases <luisgrases@gmail.com>
2015-12-18 00:02:28 -0600

    Make transition work

--- src/app/events/event-detail/event-detail.controller.js
+++ src/app/events/event-detail/event-detail.controller.js
@@ -12,10 +12,15 @@
     var vm = this;
     vm.currentEvent = EventsService.get($stateParams.eventId);
     vm.goToEdit = goToEdit;
+    vm.goToInvite = goToInvite;
     ////////////////
 
     function goToEdit(){
       $state.go("tab.events.edit", {eventId: vm.currentEvent.id});
+    }
+
+    function goToInvite(){
+      $state.go("tab.events.extrainvite", {eventId: vm.currentEvent.id});
 
     }
   }
