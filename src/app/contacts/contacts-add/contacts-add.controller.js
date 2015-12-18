918659ee8b43395f0c5dd57fc44d886ac59d875c
Luis Grases <luisgrases@gmail.com>
2015-12-18 01:05:39 -0600

    Configure controller

---
+++ src/app/contacts/contacts-add/contacts-add.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.contacts.add')
+    .controller('ContactsAddController', ContactsAddController);
+
+  ContactsController.$inject = ['ContactsService'];
+
+  /* @ngInject */
+  function ContactsAddController(ContactsService) {
+    var vm = this;
+    ////////////////
+  }
+})();
