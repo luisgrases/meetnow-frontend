fc29ee428498d83a3e0bc17ae459149a92654657
Luis Grases <luisgrases@gmail.com>
2015-12-18 00:23:07 -0600

    Add basic html and load sample contacts

--- src/app/events/event-detail/events-detail-invite/events-detail-invite.controller.js
+++ src/app/events/event-detail/events-detail-invite/events-detail-invite.controller.js
@@ -5,11 +5,12 @@
     .module('app.events.detail.invite')
     .controller('EventsDetailInviteController', EventsDetailInviteController);
 
-  EventsDetailInviteController.$inject = ['EventsService', '$stateParams'];
+  EventsDetailInviteController.$inject = ['EventsService', '$stateParams', 'ContactsService'];
 
   /* @ngInject */
-  function EventsDetailInviteController(EventsService, $stateParams) {
+  function EventsDetailInviteController(EventsService, $stateParams, ContactsService) {
     var vm = this;
+    vm.contacts = ContactsService.all();
     vm.currentEvent = EventsService.get($stateParams.eventId);
 
 
