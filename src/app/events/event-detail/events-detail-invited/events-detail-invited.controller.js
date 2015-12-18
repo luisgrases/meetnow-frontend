5b4fc8206defa729f6a43e1bcf8b7419c9a4f5d5
Luis Grases <luisgrases@gmail.com>
2015-12-18 12:17:37 -0600

    Configure controller

---
+++ src/app/events/event-detail/events-detail-invited/events-detail-invited.controller.js
@@ -0,0 +1,17 @@
+(function(){
+
+  angular
+    .module('app.events.detail.invited')
+    .controller('EventsDetailInvitedController', EventsDetailInvitedController);
+
+  EventsDetailInvitedController.$inject = ['EventsService', '$stateParams', 'ContactsService'];
+
+  /* @ngInject */
+  function EventsDetailInvitedController(EventsService, $stateParams, ContactsService) {
+    var vm = this;
+    vm.currentEvent = EventsService.get($stateParams.eventId);
+    vm.invitedStatus = $stateParams.statusId;
+    ////////////////
+  }
+
+})();
