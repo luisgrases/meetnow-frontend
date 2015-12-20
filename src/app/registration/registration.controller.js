fad7107ee6c37e3065dda34637075c68ea9daeae
Luis Grases <luisgrases@gmail.com>
2015-12-20 00:07:12 -0600

    Configure controller

---
+++ src/app/registration/registration.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+  .module('app.registration')
+  .controller('RegistrationController', RegistrationController);
+
+  RegistrationController.$inject = [];
+
+  /* @ngInject */
+  function RegistrationController() {
+    var vm = this;
+
+  }
+})();
