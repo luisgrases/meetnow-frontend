98fd3cdbaa92c0db2e3a21fe77c8c7aa5efca490
Luis Grases <luisgrases@gmail.com>
2015-12-23 01:04:18 -0600

    Add session service

---
+++ src/app/session/session.service.js
@@ -0,0 +1,19 @@
+(function() {
+
+'use strict';
+angular
+    .module('app.session', [])
+    .service('Session', function () {
+      var vm = this;
+  this.create = function (sessionId, userId, userRole) {
+    vm.id = sessionId;
+    vm.userId = userId;
+    vm.userRole = userRole;
+  };
+  vm.destroy = function () {
+    vm.id = null;
+    vm.userId = null;
+    vm.userRole = null;
+  };
+});
+})();
