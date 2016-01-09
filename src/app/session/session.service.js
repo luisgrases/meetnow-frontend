06ed4cc8abb4400fadf849faa038068d74dd84d6
Luis Grases <luisgrases@gmail.com>
2016-01-09 06:01:25 +0100

    Add activeSession method

--- src/app/session/session.service.js
+++ src/app/session/session.service.js
@@ -5,11 +5,20 @@ angular
     .module('app.session', [])
     .service('Session', function () {
       var vm = this;
+
   vm.create = function (userId) {
     vm.id = userId;
   };
+
   vm.destroy = function () {
     vm.id = null;
   };
+
+  vm.activeSession = function () {
+    if (vm.id == null) {
+      return false;
+    }
+    return true;
+  }
 });
 })();
