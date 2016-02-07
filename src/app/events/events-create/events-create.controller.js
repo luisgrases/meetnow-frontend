8ea1b7bee2467204dc653a9086a98129c7191fe8
Luis Grases <luisgrases@gmail.com>
2016-02-07 11:57:02 -0600

    Clear forms when leaving view

--- src/app/events/events-create/events-create.controller.js
+++ src/app/events/events-create/events-create.controller.js
@@ -5,10 +5,10 @@
     .module('app.events.create')
     .controller('EventsCreateController', EventsCreateController);
 
-  EventsCreateController.$inject = ['EventsService', '$state', '$ionicHistory'];
+  EventsCreateController.$inject = ['EventsService', '$state', '$ionicHistory', 'ContactsService'];
 
   /* @ngInject */
-  function EventsCreateController(EventsService, $state, $ionicHistory) {
+  function EventsCreateController(EventsService, $state, $ionicHistory, ContactsService) {
     var vm = this;
     vm.goToInvitePeopleView = goToInvitePeopleView;
     vm.create = create;
@@ -17,9 +17,25 @@
     vm.increaseAssistLimit = increaseAssistLimit;
     vm.decreaseAssistLimit = decreaseAssistLimit;
     activate();
+
     ////////////////
 
     function activate() {
+      emptyNewEvent();
+      unhideAllContacts();
+    }
+
+    function emptyNewEvent() {
+      EventsService.newEvent = {
+        assist_limit: 0,
+        users: []
+      };
+    }
+
+    function unhideAllContacts(){
+      angular.forEach(ContactsService.all, function(contact){
+        contact.hide = false;
+      })
     }
 
 
